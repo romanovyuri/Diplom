2a12802badfd01c172715f47a073437c8b896475
Yuri.Romanov <romanovyp@mail.ru>
2022-10-04 00:51:06 +0500

    Diplom 2

--- terraform/nginx_install.tf
+++ terraform/nginx_install.tf
@@ -1,4 +1,13 @@
-# Reverse proxy
+resource "null_resource" "wait" {
+  provisioner "local-exec" {
+    command = "sleep 100"
+  }
+
+  depends_on = [
+    local_file.inventory
+  ]
+}
+
 resource "null_resource" "nginx" {
   provisioner "local-exec" {
     command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../ansible/inventory ../ansible/nginx_install.yml"
@@ -8,13 +17,3 @@ resource "null_resource" "nginx" {
     null_resource.wait
   ]
 }
-
-#resource "null_resource" "nginx_ssh" {
- # provisioner "local-exec" {
-  #  command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../ansible/inventory ../ansible/proxy_ssh.yml --extra-vars 'username=${var.username} destfile=files/id_rsa.pub srcfile=files/id_rsa.pub'"
-  #}
-
-  #depends_on = [
-   # null_resource.proxy
-  #]
-#}
