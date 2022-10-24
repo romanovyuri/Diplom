1aadbd57e84cbef85fe3b56427ab55fb8b01bcb4
Yuri.Romanov <romanovyp@mail.ru>
2022-10-24 23:43:55 +0500

    Fixed bugs

--- terraform/network.tf
+++ terraform/network.tf
@@ -35,20 +35,6 @@ resource "yandex_vpc_subnet" "subnet_zone_a_private" {
   route_table_id = yandex_vpc_route_table.route_table_nat.id
   }
 
-/*# Создаем подсеть в зоне B для Public
-resource "yandex_vpc_subnet" "subnet_zone_b_public" {
-  zone       = "ru-central1-b"
-  network_id = yandex_vpc_network.default.id
-  v4_cidr_blocks = ["192.168.20.0/24"]
-}
-
-# Создаем подсеть в зоне B для Private
-resource "yandex_vpc_subnet" "subnet_zone_b_private" {
-  zone           = "ru-central1-b"
-  network_id     = yandex_vpc_network.default.id
-  v4_cidr_blocks = ["192.168.21.0/24"]
-}*/
-
 # Создается запись в зоне для домена, после создания NGINX
 data "yandex_dns_zone" "zone" {
   name = "devops-netology"
